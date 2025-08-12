4383d29ff98135705f7ccfa77d1a4a5f899f4f3d
NStefan002 <n.stefan002@gmail.com>
2025-08-12 04:23:10 +0200

    test: add `vblock` and `vline` tests

--- spec/surround_spec.lua
+++ spec/surround_spec.lua
@@ -10,14 +10,21 @@ describe("visual-surround", function()
             "foo bar",
             "baz qux",
         })
+        api.nvim_win_set_cursor(0, { 1, 0 })
+    end)
+
+    after_each(function()
+        vs.setup() -- reset to defaults
     end)
 
     describe("surround functionality", function()
-        it("add and remove surround in visual mode", function()
+        before_each(function()
             vs.setup({ exit_visual_mode = false, enable_wrapped_deletion = true })
+        end)
+
+        it("add and remove surround in visual mode", function()
             -- simulate visual selection
-            api.nvim_win_set_cursor(0, { 1, 1 })
-            vim.cmd("normal viw")
+            vim.cmd("normal! viw")
             -- surround with ()
             vs.surround("(")
             local line = api.nvim_get_current_line()
@@ -26,6 +33,40 @@ describe("visual-surround", function()
             vs.surround("(")
             line = api.nvim_get_current_line()
             eq("foo bar", line)
+        end)
+
+        it("add and remove surround in visual block mode", function()
+            -- simulate visual selection
+            vim.cmd("normal! Vj")
+            -- surround with ()
+            vs.surround("(")
+            local lines = api.nvim_buf_get_lines(0, 0, -1, false)
+            eq({ "(foo bar", "baz qux)" }, lines)
+            -- move one line up
+            vim.cmd("normal! k")
+            -- surround with ()
+            vs.surround("(")
+            lines = api.nvim_buf_get_lines(0, 0, -1, false)
+            eq({ "((foo bar)", "baz qux)" }, lines)
+            -- move one line down
+            vim.cmd("normal! j")
+            -- remove surround
+            vs.surround("(")
+            lines = api.nvim_buf_get_lines(0, 0, -1, false)
+            eq({ "(foo bar)", "baz qux" }, lines)
+        end)
+
+        it("add and remove surround in visual line mode", function()
+            -- simulate visual selection
+            vim.cmd(("normal! %sjl"):format(api.nvim_replace_termcodes("<c-v>", true, false, true)))
+            -- surround with ()
+            vs.surround("(")
+            local lines = api.nvim_buf_get_lines(0, 0, -1, false)
+            eq({ "(fo)o bar", "(ba)z qux" }, lines)
+            -- remove surround
+            vs.surround("(")
+            lines = api.nvim_buf_get_lines(0, 0, -1, false)
+            eq({ "foo bar", "baz qux" }, lines)
         end)
     end)
 
